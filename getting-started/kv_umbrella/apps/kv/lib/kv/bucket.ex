a2f057a4064127d9c438c9aa1c449e7d69c0ec53
Aslak Johansen <aslakj@gmail.com>
2021-05-01 18:30:34 +0200

    Added a copy of the kv project

---
+++ getting-started/kv_umbrella/apps/kv/lib/kv/bucket.ex
@@ -0,0 +1,39 @@
+defmodule KV.Bucket do
+    use Agent, restart: :temporary
+    
+    @doc """
+    Starts a new bucket.
+    """
+    def start_link(_opts) do
+        Agent.start_link(fn -> %{} end)
+    end
+    
+    @doc """
+    Gets a value from the `bucket` by `key`.
+    """
+    def get(bucket, key) do
+        Agent.get(bucket, &Map.get(&1, key))
+    end
+    
+    @doc """
+    Puts the `balue` for the given `key` in the `bucket`.
+    """
+    def put(bucket, key, value) do
+        Agent.update(bucket, fn state ->
+            Map.put(state, key, value)
+        end)
+    end
+    
+    @doc """
+    Deletes `key`from `bucket`.
+    
+    Returns the current value of `key`, if `key` exists.
+    """
+    def delete(bucket, key) do
+        Agent.get_and_update(bucket, fn dict ->
+            Map.pop(dict, key)
+        end)
+    end
+end
+
+
---
+++ getting-started/kv_umbrella/apps/kv/test/kv_test.exs
@@ -0,0 +1,6 @@
+defmodule KVTest do
+  use ExUnit.Case
+  doctest KV
+
+end
+
