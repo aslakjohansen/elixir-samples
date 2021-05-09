9bdf663210f921361ad7e53dd1e05d2945c43666
Aslak Johansen <aslakj@gmail.com>
2021-05-09 11:56:46 +0200

    Added form

--- phoenix-todo/live_view_todos/lib/live_view_todos_web/live/todo_live.ex
+++ phoenix-todo/live_view_todos/lib/live_view_todos_web/live/todo_live.ex
@@ -1,11 +1,12 @@
 defmodule LiveViewTodosWeb.TodoLive do
     use LiveViewTodosWeb, :live_view # macro for importing boilerplate stuff
+    alias LiveViewTodos.Todos
     
     def mount(params, session, socket) do
-        {:ok, socket}
+        {:ok, assign(socket, todos: Todos.list_todos())}
     end
     
-    def render(assigns) do
-        ~L"Rendering LiveView"
-    end
+#    def render(assigns) do
+#        ~L"Rendering LiveView"
+#    end
 end
