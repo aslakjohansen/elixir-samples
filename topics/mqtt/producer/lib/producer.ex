0f41fe94ef029b4e020a3868d98b53ad977bd661
Aslak Johansen <aslakj@gmail.com>
2021-08-03 00:24:56 +0200

    Something happens periodically

--- topics/mqtt/producer/lib/producer.ex
+++ topics/mqtt/producer/lib/producer.ex
@@ -1,18 +1,31 @@
 defmodule Producer do
-  @moduledoc """
-  Documentation for `Producer`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Producer.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+    use GenServer
+    
+    # behaviour functions
+    
+    def start_link(args) do
+        GenServer.start(__MODULE__, args)
+    end
+    
+    # callback functions
+    
+    @impl true
+    def init(_) do
+        ref = initiate_countdown(1_000)
+        {:ok, %{ref: ref}}
+    end
+    
+    @impl true
+    def handle_info(:timeout, state) do
+        IO.puts("Timeout called #{DateTime.utc_now()}")
+        ref = initiate_countdown(1_000)
+        new_state = Map.put(state, :ref, ref)
+        {:noreply, new_state}
+    end
+    
+    # helper functions
+    
+    defp initiate_countdown(timeout) do
+        Process.send_after(self(), :timeout, timeout)
+    end
 end
